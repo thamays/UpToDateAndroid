4982b96a80d66d8efb08f3904a8568172a836f62
Thamays <gthamays@gmail.com>
2024-07-14 00:06:54 -0400

    Unit 3: Display lists and use Material Design - Theme and Animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,10 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
     testImplementation("junit:junit:4.13.2")
