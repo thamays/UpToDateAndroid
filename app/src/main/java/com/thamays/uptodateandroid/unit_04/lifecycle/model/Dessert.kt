9dad7479fe379c40c34596889bf86b56fbeff9fe
Thamays <gthamays@gmail.com>
2024-07-14 15:08:34 -0400

    Unit 4: Navigation and app architecture - Lifecycle

---
+++ app/src/main/java/com/thamays/uptodateandroid/unit_04/lifecycle/model/Dessert.kt
@@ -0,0 +1,6 @@
+package com.thamays.uptodateandroid.unit_04.lifecycle.model
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
